35d14a37c1b56c33d95d97a3b096e0edc3cfa897
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-01 18:21:28 -0400

    Cleanup

--- the-v2-addon/tests/amd-hax.ts
+++ the-v2-addon/tests/amd-hax.ts
@@ -8,6 +8,3 @@ Ember.__loader.require = globalThis.require;
 Ember.__loader.has = globalThis.has;
 
 
-// import { registerHelper, registerAsyncHelper, registerWaiter, unregisterWaiter, unregisterHelper } from 'ember-testing/lib/helpers/test/helpers';
-
-// Ember.Test ||= { registerHelper, registerAsyncHelper, registerWaiter, unregisterHelper, unregisterWaiter}
